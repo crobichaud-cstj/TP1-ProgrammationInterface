e81465ff34b35550c105e58c7e697267fd0a6a50
Cedrick <2150658@cstj.qc.ca>
2022-09-30 14:23:40 -0400

    asd

--- 3C4_TP1/3C4_TP1/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ 3C4_TP1/3C4_TP1/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace _3C4_TP1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/3C4_TP1;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/3C4_TP1;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
