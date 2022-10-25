6d12fbc30ef35cb8485c246f9a0c26f19a9e59e4
LFournier7 <2181193@cstj.qc.ca>
2022-10-24 20:52:42 -0400

    gestionCours

--- 3C4_TP1/3C4_TP1/GestionCours.xaml.cs
+++ 3C4_TP1/3C4_TP1/GestionCours.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,10 @@ namespace _3C4_TP1
     {
         Course currentCourse = new Course();
         Evaluation currentEva = new Evaluation();
+        Student currentStudent = new Student();
+
+        List<Student> students = new List<Student>();   
+
         int totalExam = 0;
         int nbExam = 0;
         public GestionCours(Course course)
@@ -37,14 +42,15 @@ namespace _3C4_TP1
             }
             comboboxEva.SelectedIndex = 0;
 
-            UpdateOnExam();
+            updateOnExam();
             
         }
 
 
 
-        private void UpdateOnExam()
+        private void updateOnExam()
         {
+            students.Clear();
             listStud.Items.Clear();
             Ponde.Text = currentEva.Value.ToString();
 
@@ -53,16 +59,33 @@ namespace _3C4_TP1
 
             foreach (var s in currentCourse.StudentIds)
             {
-                string str = s.ToString() + " - ";
-                str += App.Current.Students[s].FirstName + ", ";
-                str += App.Current.Students[s].LastName + " ";
-                str += App.Current.Students[s].LastName + " - ";
-                str += +currentEva.StudentResults[s];
-                totalExam += currentEva.StudentResults[s];
-                nbExam++;
-                listStud.Items.Add(str);
+                if (currentEva.StudentResults[s] != null)
+                {
+                    students.Add(App.Current.Students[s]);
+                    string str = s.ToString() + " - ";
+                    str += App.Current.Students[s].FirstName + ", ";
+                    str += App.Current.Students[s].LastName + " ";
+                    str += App.Current.Students[s].LastName + " - ";
+                    str += +currentEva.StudentResults[s];
+                    totalExam += currentEva.StudentResults[s];
+                    nbExam++;
+                    listStud.Items.Add(str);
+                }
+                
             }
+            currentStudent = students[0];
+            listStud.SelectedIndex = 0;
+
             Moyen.Text = (totalExam / nbExam).ToString();
+
+            updateOnStudent();
+        }
+
+        private void updateOnStudent()
+        {
+            Iden.Text = currentStudent.Id.ToString();
+            Nom.Text = currentStudent.LastName;
+            Prenom.Text = currentStudent.FirstName;
         }
 
         private void comboboxEva_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -74,7 +97,19 @@ namespace _3C4_TP1
                     currentEva = eva;
                 }
             }
-            UpdateOnExam();
+            updateOnExam();
+        }
+
+        private void listStud_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (Student stu in students)
+            {
+                if (stu.Id.ToString() == listStud.SelectedItem.ToString().Take(8))
+                {
+                    currentStudent = stu;
+                }
+            }
+            updateOnStudent();
         }
     }
 }
